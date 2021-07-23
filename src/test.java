3cc383b9a5f00a850d4545a60b59c76fb3a133b1
kiranthomasfico <87848548+kiranthomasfico@users.noreply.github.com>
2021-07-23 16:01:00 +0530

    all

---
+++ src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean var = true ;
+		
+		while (var != false) {
+			
+			System.out.println("Infinite Loop !! ");
+		}
+	}
+
+}
